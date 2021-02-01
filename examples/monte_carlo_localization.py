24b1076ed04c4a9607c59c74b85268a56b38aa87
Andrew Presland <andrew.presland@gmail.com>
2021-02-01 16:45:30 +0100

    feat: Window for creating location fixes and displaying results
    Location fixes will be created by capturing mouse cursor locations so we need a window for that.

--- examples/monte_carlo_localization.py
+++ examples/monte_carlo_localization.py
@@ -1,6 +1,11 @@
 import numpy as np
+import cv2
 from numpy.random import uniform
 from localization import MonteCarloLocalization
+
+window_height =800
+window_width =800
+window_name ="Monte Carlo Localization"
 
 def uniform_prior(N):
     xs = np.array([0,800])
@@ -12,13 +17,23 @@ def uniform_prior(N):
 
 prior_fn = uniform_prior(200)
 
-def example_mcl():
+def localization():
+
     # create filter
     filter = MonteCarloLocalization(
         prior_fn=prior_fn,
         n_particles=200)
 
+    # blank image
+    img = np.zeros((window_height, window_width,3), np.uint8)
+
+    # create window
+    cv2.namedWindow(window_name)
+    cv2.imshow(window_name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 if __name__ == "__main__":
 
     print("+++ running localization example +++")
-    example_mcl()
+    localization()
